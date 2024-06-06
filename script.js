6d4ac2032f16a0f74a15157c9eef83fd73ce01c8
JAHONGIR-AMONOV <jahongir.amonov3010@gmail.com>
2024-06-06 16:50:11 +0500

    answer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let name ='Alex'.trim()
 let money ='10000'
 let account =7777
 
-let answer = prompt("Как вас зовут?")
+let answer = prompt("Как вас зовут?").toLowerCase()
 
 if(answer ===name){
     let secondAnswer = prompt('Номер счета?')

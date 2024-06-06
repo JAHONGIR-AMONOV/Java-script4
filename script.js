f1eb7eb352eb1071fa20fb59ad74c7b55cd2d94a
JAHONGIR-AMONOV <jahongir.amonov3010@gmail.com>
2024-06-06 16:38:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let name ='Alex'
+let money ='10000'
+let account =7777
+
+let answer = prompt("Как вас зовут?")
+
+if(answer ===name){
+    let secondAnswer = prompt('Номер счета?')
+
+    if(secondAnswer ===account) {
+        let thirdAnswer =prompt('Сколько обналичить?')
+        if(money <=10000) {
+            alert('Все отлично');
+            
+        } else {
+          alert('Недостаточно средств')
+        }
+    } else {
+        alert('Пользователь не найден, досвидули')
+    }
+}
+ else {
+    alert('Пользователь не найден, досвидули')
+}
